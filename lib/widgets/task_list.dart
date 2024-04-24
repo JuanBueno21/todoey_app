627eb99d120a67fd50249aa45d582c8a160ed950
JuanBueno21 <juancamilobuenomintic@gmail.com>
2024-04-24 11:01:23 -0500

    [todoey] implement checkBox

--- lib/widgets/task_list.dart
+++ lib/widgets/task_list.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:todoey_app/widgets/task_tile.dart';
+import 'package:todoey_app/models/task.dart';
 
-class TasksList extends StatelessWidget {
+class TasksList extends StatefulWidget {
   const TasksList({super.key});
 
+  @override
+  State<TasksList> createState() => _TasksListState();
+}
+
+class _TasksListState extends State<TasksList> {
+  List<Task> tasks = [
+    Task(name: "Buy milk"),
+    Task(name: "Buy eggs"),
+    Task(name: "Buy bread"),
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: const <Widget>[
-        TaskTile(),
-        TaskTile(),
-        TaskTile()
-      ],
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return TaskTile(
+          isChecked: tasks[index].isDone,
+          taskTitle: tasks[index].name,
+          checkboxCallback: (checkBoxState) {
+            setState(() {
+              tasks[index].toggleDone();
+            });
+          },
+
+        );
+      },
+      itemCount: tasks.length,
     );
   }
 }
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 class TaskTile extends StatelessWidget {
-  const TaskTile({super.key});
 
+  final bool isChecked;
+  final String taskTitle;
+  final Function(bool?) checkboxCallback;
+
+  const TaskTile({super.key, required this.isChecked, required this.taskTitle, required this.checkboxCallback});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: const Text("This is a task"),
-      trailing: Checkbox(value: false,
-          onChanged: (bool? value) {}),
+      title: Text(
+        taskTitle,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: Checkbox(
+        activeColor: Colors.lightBlueAccent,
+        value: isChecked,
+        onChanged: checkboxCallback,
+      ),
     );
   }
 }
